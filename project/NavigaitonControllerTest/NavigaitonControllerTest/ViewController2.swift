2189f25d51c8808dbdd51519ba639553c4fa0bea
Adem Koyan <ademkoyan@gmail.com>
2023-01-10 14:45:56 +0300

    Add files via upload

---
+++ project/NavigaitonControllerTest/NavigaitonControllerTest/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  NavigaitonControllerTest
+//
+//  Created by Adem KOYAN on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
