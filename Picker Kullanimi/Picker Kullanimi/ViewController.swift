295be9372c5a29618634bb5d71a8cf2c5568fbe8
ademkoyan <ademkoyan@gmail.com>
2023-01-15 01:51:13 +0300

    programmatic uı ile textview eklendi

--- Picker Kullanimi/Picker Kullanimi/ViewController.swift
+++ Picker Kullanimi/Picker Kullanimi/ViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let datePicker: UITextView = {
+        let textView = UITextView()
+        
+        let attrText = NSMutableAttributedString(string: "Bişiyler bişiyler", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)])
+        textView.attributedText = attrText
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+    } ()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World")
-        // yeni deneme
+        view.backgroundColor = .blue
+        view.addSubview(datePicker)
+       
+        addConstraite()
     }
 
+    private func addConstraite() {
+        
+        datePicker.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        datePicker.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300).isActive = true
 
+    }
 }
 
