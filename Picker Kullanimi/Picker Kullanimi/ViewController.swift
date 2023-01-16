2d09b08617899228b47f41c0fb17d78b1471d7a9
ademkoyan <ademkoyan@gmail.com>
2023-01-16 13:31:47 +0300

    textview ile datePicker kullanımı

--- Picker Kullanimi/Picker Kullanimi/ViewController.swift
+++ Picker Kullanimi/Picker Kullanimi/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let datePicker: UITextView = {
+    let datePickerTxt: UITextView = {
         let textView = UITextView()
         
         let attrText = NSMutableAttributedString(string: "Bişiyler bişiyler", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)])
@@ -20,23 +20,49 @@ class ViewController: UIViewController {
         return textView
     } ()
     
+    var datePicker: UIDatePicker?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World")
         view.backgroundColor = .blue
-        view.addSubview(datePicker)
+        view.addSubview(datePickerTxt)
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePickerTxt.inputView = datePicker
+        
+        datePicker?.addTarget(self, action: #selector(tarihGoster(datePicker:)), for:.valueChanged )
+       
+        let dokunmaAlgilama = UIGestureRecognizer(target: self, action: #selector(dokunmaAlgilamaMetod)){
+            view.gestureRecognizers(dokunmaAlgilama())
+        }
        
         addConstraite()
     }
 
     private func addConstraite() {
         
-        datePicker.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
-        datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        datePicker.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300).isActive = true
+        datePickerTxt.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        datePickerTxt.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        datePickerTxt.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        datePickerTxt.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300).isActive = true
 
     }
+    @objc func tarihGoster(datePicker: UIDatePicker) {
+        let dateFortmatter = DateFormatter()
+        dateFortmatter.dateFormat = "MM/dd/yyyy"
+        let alinanTarih = dateFortmatter.string(from: datePicker.date)
+        
+        datePickerTxt.text = alinanTarih
+        
+        view.endEditing(true)
+    }
+    
+    @objc func dokunmaAlgilamametod() {
+        view.endEditing(true)
+    }
+    
 }
 
