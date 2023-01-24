bd12cba3d19d04193bec18f42e92acc4e917279e
ademkoyan <ademkoyan@gmail.com>
2023-01-24 23:49:38 +0300

    bişiyler bişiyler

---
+++ O Usage/O UsageUITests/O_UsageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  O_UsageUITestsLaunchTests.swift
+//  O UsageUITests
+//
+//  Created by Adem KOYAN on 24.01.2023.
+//
+
+import XCTest
+
+final class O_UsageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
