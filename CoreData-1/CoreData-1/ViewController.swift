b1458ae2b56f3eeff3104b4286def67e4a930e19
ademkoyan <ademkoyan@gmail.com>
2023-02-09 22:10:23 +0300

    CoreData installation

---
+++ CoreData-1/CoreData-1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CoreData-1
+//
+//  Created by Adem KOYAN on 9.02.2023.
+//
+
+import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+class ViewController: UIViewController {
+    
+    let context = appDelegate.persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
